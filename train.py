7f7ea17cfcc245aa78540e74052873abd8e23851
joao.live2002@gmail.com <joao.live2002@gmail.com>
2022-09-17 14:10:42 +0000

    databricks setec

---
+++ train.py
@@ -0,0 +1,46 @@
+# Databricks notebook source
+# DBTITLE 1,imports
+from sklearn import tree
+from sklearn import model_selection
+from sklearn import linear_model
+from sklearn import ensemble
+from sklearn import metrics
+
+import mlflow
+
+# COMMAND ----------
+
+# DBTITLE 1,Dados
+print("Obtendo dasdo....")
+
+df = spark.table("sandbox_apoiadores.abt_dota_pre_match").toPandas()
+df
+
+# COMMAND ----------
+
+# DBTITLE 1,Setup do experimento
+exp_name = "/Users/joao.live2002@gmail.com/fatec_dota joaoP"
+mlflow.set_experiment(exp_name)
+
+# COMMAND ----------
+
+# DBTITLE 1,Definições de coluna
+target = "radiant_win"
+id_column = "match_id"
+
+features = list(set(df.columns) - set([target, id_colum]))
+
+# COMMAND ----------
+
+# DBTITLE 1,Split de dados
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    df[features],
+    df[target],
+    test_size=0.2,
+    random_state=42)
+
+# COMMAND ----------
+
+print("Geral: ", df[target], mean())
+print("Treino: ", y_train.mean())
+print("Teste: ", y_test.mean())
